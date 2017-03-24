f4aa849feec251dfaac5ce3a4397bb8db2da686e
didier.putman <didier.putman@e-doceo.net>
2017-03-24 13:13:09 +0100

    Factorisation affichage hexagone

--- index.php
+++ index.php
@@ -9,45 +9,14 @@
 <table width="100%">
     <tr>
         <td width="50%">
-            <div>
-                <div class="puzzle">
-                    <div>
-                        <div class="hexagon a"><span class="index_puzzle" id="a_index" data-id="a">??</span></div>
-                        <div class="hexagon b"><span class="index_puzzle" id="b_index" data-id="b">??</span></div>
-                        <div class="hexagon c"><span class="index_puzzle" id="c_index" data-id="c">??</span></div>
-                    </div>
-                    <div>
-                        <div class="hexagon d"><span class="index_puzzle" id="d_index" data-id="d">??</span></div>
-                        <div class="hexagon e"><span class="index_puzzle" id="e_index" data-id="e">??</span></div>
-                        <div class="hexagon f"><span class="index_puzzle" id="f_index" data-id="f">??</span></div>
-                        <div class="hexagon g"><span class="index_puzzle" id="g_index" data-id="g">??</span></div>
-                    </div>
-                    <div>
-                        <div class="hexagon h"><span class="index_puzzle" id="h_index" data-id="h">??</span></div>
-                        <div class="hexagon i"><span class="index_puzzle" id="i_index" data-id="i">??</span></div>
-                        <div class="hexagon j"><span class="index_puzzle" id="j_index" data-id="j">??</span></div>
-                        <div class="hexagon k"><span class="index_puzzle" id="k_index" data-id="k">??</span></div>
-                        <div class="hexagon l"><span class="index_puzzle" id="l_index" data-id="l">??</span></div>
-                    </div>
-                    <div>
-                        <div class="hexagon m"><span class="index_puzzle" id="m_index" data-id="m">??</span></div>
-                        <div class="hexagon n"><span class="index_puzzle" id="n_index" data-id="n">??</span></div>
-                        <div class="hexagon o"><span class="index_puzzle" id="o_index" data-id="o">??</span></div>
-                        <div class="hexagon p"><span class="index_puzzle" id="p_index" data-id="p">??</span></div>
-                    </div>
-                    <div>
-                        <div class="hexagon q"><span class="index_puzzle" id="q_index" data-id="q">??</span></div>
-                        <div class="hexagon r"><span class="index_puzzle" id="r_index" data-id="r">??</span></div>
-                        <div class="hexagon s"><span class="index_puzzle" id="s_index" data-id="s">??</span></div>
-                    </div>
-                </div>
+            <div class="puzzle">
             </div>
         </td>
         <td width="50%">
             <div class="result">
                 <div class="search" style="display: none;"><span>Recherche en cours...</span></div>
                 <div class="count" style="display: none;"><span>Nombre de solution trouv&eacute;e : </span><span id="count"></span></div>
-                <div class="ms" style="display: none;"><span>Temps d&apos;exécution (s) : </span><span id="ms"></span></div>
+                <div class="ms" style="display: none;"><span>Temps d&apos;ex&eacute;cution (s) : </span><span id="ms"></span></div>
                 <br/>
                 <div class="consigne"><span>Cliquer sur un hexagone.</span></div>
             </div>
--- resolve.js
+++ resolve.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('span.index_puzzle').bind('click', function(event){
-		spanClick(event, $(event.target).data('id'));
+		spanClick(event, $(event.target).attr('id'));
 	});
 });
 
@@ -8,24 +8,36 @@ var pas_top = -28;
 var pas_left = 51;
 var pos_top = 100;
 var pos_left = 100;
+var line = 0;
 for(var i = 1 ; i < 20 ; i++){
 	var char_code = i + 96;
-	if (char_code == 100 ) { //d
+	var code = String.fromCharCode(char_code);
+	var hexa_html = '<div class="hexagon ' + code + '"><span class="index_puzzle" id="' + code + '">??</span></div>';
+	if (char_code == 97 ) { //a
+		$('div.puzzle').append('<div id="line' + (++line).toString() +'"></div>');
+		pos_left = 100;
+		pos_top = 100;
+	} else if (char_code == 100 ) { //d
+		$('div.puzzle').append('<div id="line' + (++line).toString() +'"></div>');
 		pos_left = 75;
 		pos_top = 60;
 	} else if (char_code == 104) { //h
+		$('div.puzzle').append('<div id="line' + (++line).toString() +'"></div>');
 		pos_left = 50;
 		pos_top = -8;
 	} else if (char_code == 109) { //m
+		$('div.puzzle').append('<div id="line' + (++line).toString() +'"></div>');
 		pos_left = 77;
 		pos_top = -104;
 	} else if (char_code == 113) { //q
+		$('div.puzzle').append('<div id="line' + (++line).toString() +'"></div>');
 		pos_left = 104;
 		pos_top = -172;
 	}
-	$('.hexagon.' + String.fromCharCode(char_code)).css({left: pos_left + 'px', top: pos_top + 'px'});
-	pos_left = pos_left + pas_left;
-	pos_top = pos_top + pas_top;
+	$('div#line' + line.toString()).append(hexa_html);
+	$('.hexagon.' + code).css({left: pos_left + 'px', top: pos_top + 'px'});
+	pos_left += pas_left;
+	pos_top += pas_top;
 }
 
 function compute(position){
@@ -75,25 +87,25 @@ function printPuzzle(combinaison, position) {
 	 q + r + s         = 38
 	 */
 	if (position == 'b') {
-		$('#a_index').text(combinaison.a);
-		$('#b_index').text(combinaison.b);
-		$('#c_index').text(combinaison.c);
-		$('#d_index').text(combinaison.d);
-		$('#e_index').text(combinaison.e);
-		$('#f_index').text(combinaison.f);
-		$('#g_index').text(combinaison.g);
-		$('#h_index').text(combinaison.h);
-		$('#i_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#k_index').text(combinaison.k);
-		$('#l_index').text(combinaison.l);
-		$('#m_index').text(combinaison.m);
-		$('#n_index').text(combinaison.n);
-		$('#o_index').text(combinaison.o);
-		$('#p_index').text(combinaison.p);
-		$('#q_index').text(combinaison.q);
-		$('#r_index').text(combinaison.r);
-		$('#s_index').text(combinaison.s);
+		$('#a').text(combinaison.a);
+		$('#b').text(combinaison.b);
+		$('#c').text(combinaison.c);
+		$('#d').text(combinaison.d);
+		$('#e').text(combinaison.e);
+		$('#f').text(combinaison.f);
+		$('#g').text(combinaison.g);
+		$('#h').text(combinaison.h);
+		$('#i').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#k').text(combinaison.k);
+		$('#l').text(combinaison.l);
+		$('#m').text(combinaison.m);
+		$('#n').text(combinaison.n);
+		$('#o').text(combinaison.o);
+		$('#p').text(combinaison.p);
+		$('#q').text(combinaison.q);
+		$('#r').text(combinaison.r);
+		$('#s').text(combinaison.s);
 	} else if (position == 'g') {
 		/*
 		 a + b + c         = 38
@@ -102,25 +114,25 @@ function printPuzzle(combinaison, position) {
 		 m + n + o + p     = 38
 		 q + r + s         = 38
 		 */
-		$('#c_index').text(combinaison.a);
-		$('#g_index').text(combinaison.b);
-		$('#l_index').text(combinaison.c);
-		$('#b_index').text(combinaison.d);
-		$('#f_index').text(combinaison.e);
-		$('#k_index').text(combinaison.f);
-		$('#p_index').text(combinaison.g);
-		$('#a_index').text(combinaison.h);
-		$('#e_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#o_index').text(combinaison.k);
-		$('#s_index').text(combinaison.l);
-		$('#d_index').text(combinaison.m);
-		$('#i_index').text(combinaison.n);
-		$('#n_index').text(combinaison.o);
-		$('#r_index').text(combinaison.p);
-		$('#h_index').text(combinaison.q);
-		$('#m_index').text(combinaison.r);
-		$('#q_index').text(combinaison.s);
+		$('#c').text(combinaison.a);
+		$('#g').text(combinaison.b);
+		$('#l').text(combinaison.c);
+		$('#b').text(combinaison.d);
+		$('#f').text(combinaison.e);
+		$('#k').text(combinaison.f);
+		$('#p').text(combinaison.g);
+		$('#a').text(combinaison.h);
+		$('#e').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#o').text(combinaison.k);
+		$('#s').text(combinaison.l);
+		$('#d').text(combinaison.m);
+		$('#i').text(combinaison.n);
+		$('#n').text(combinaison.o);
+		$('#r').text(combinaison.p);
+		$('#h').text(combinaison.q);
+		$('#m').text(combinaison.r);
+		$('#q').text(combinaison.s);
 	} else if (position == 'p') {
 		/*
 		 a + b + c         = 38
@@ -129,25 +141,25 @@ function printPuzzle(combinaison, position) {
 		 m + n + o + p     = 38
 		 q + r + s         = 38
 		 */
-		$('#l_index').text(combinaison.a);
-		$('#p_index').text(combinaison.b);
-		$('#s_index').text(combinaison.c);
-		$('#g_index').text(combinaison.d);
-		$('#k_index').text(combinaison.e);
-		$('#o_index').text(combinaison.f);
-		$('#r_index').text(combinaison.g);
-		$('#c_index').text(combinaison.h);
-		$('#f_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#n_index').text(combinaison.k);
-		$('#q_index').text(combinaison.l);
-		$('#b_index').text(combinaison.m);
-		$('#e_index').text(combinaison.n);
-		$('#i_index').text(combinaison.o);
-		$('#m_index').text(combinaison.p);
-		$('#a_index').text(combinaison.q);
-		$('#d_index').text(combinaison.r);
-		$('#h_index').text(combinaison.s);
+		$('#l').text(combinaison.a);
+		$('#p').text(combinaison.b);
+		$('#s').text(combinaison.c);
+		$('#g').text(combinaison.d);
+		$('#k').text(combinaison.e);
+		$('#o').text(combinaison.f);
+		$('#r').text(combinaison.g);
+		$('#c').text(combinaison.h);
+		$('#f').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#n').text(combinaison.k);
+		$('#q').text(combinaison.l);
+		$('#b').text(combinaison.m);
+		$('#e').text(combinaison.n);
+		$('#i').text(combinaison.o);
+		$('#m').text(combinaison.p);
+		$('#a').text(combinaison.q);
+		$('#d').text(combinaison.r);
+		$('#h').text(combinaison.s);
 	} else if (position == 'r') {
 		/*
 		 a + b + c         = 38
@@ -156,25 +168,25 @@ function printPuzzle(combinaison, position) {
 		 m + n + o + p     = 38
 		 q + r + s         = 38
 		 */
-		$('#s_index').text(combinaison.a);
-		$('#r_index').text(combinaison.b);
-		$('#q_index').text(combinaison.c);
-		$('#p_index').text(combinaison.d);
-		$('#o_index').text(combinaison.e);
-		$('#n_index').text(combinaison.f);
-		$('#m_index').text(combinaison.g);
-		$('#l_index').text(combinaison.h);
-		$('#k_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#i_index').text(combinaison.k);
-		$('#h_index').text(combinaison.l);
-		$('#g_index').text(combinaison.m);
-		$('#f_index').text(combinaison.n);
-		$('#e_index').text(combinaison.o);
-		$('#d_index').text(combinaison.p);
-		$('#c_index').text(combinaison.q);
-		$('#b_index').text(combinaison.r);
-		$('#a_index').text(combinaison.s);
+		$('#s').text(combinaison.a);
+		$('#r').text(combinaison.b);
+		$('#q').text(combinaison.c);
+		$('#p').text(combinaison.d);
+		$('#o').text(combinaison.e);
+		$('#n').text(combinaison.f);
+		$('#m').text(combinaison.g);
+		$('#l').text(combinaison.h);
+		$('#k').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#i').text(combinaison.k);
+		$('#h').text(combinaison.l);
+		$('#g').text(combinaison.m);
+		$('#f').text(combinaison.n);
+		$('#e').text(combinaison.o);
+		$('#d').text(combinaison.p);
+		$('#c').text(combinaison.q);
+		$('#b').text(combinaison.r);
+		$('#a').text(combinaison.s);
 	} else if (position == 'm') {
 		/*
 		 a + b + c         = 38
@@ -183,25 +195,25 @@ function printPuzzle(combinaison, position) {
 		 m + n + o + p     = 38
 		 q + r + s         = 38
 		 */
-		$('#q_index').text(combinaison.a);
-		$('#m_index').text(combinaison.b);
-		$('#h_index').text(combinaison.c);
-		$('#r_index').text(combinaison.d);
-		$('#n_index').text(combinaison.e);
-		$('#i_index').text(combinaison.f);
-		$('#d_index').text(combinaison.g);
-		$('#s_index').text(combinaison.h);
-		$('#o_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#e_index').text(combinaison.k);
-		$('#a_index').text(combinaison.l);
-		$('#p_index').text(combinaison.m);
-		$('#k_index').text(combinaison.n);
-		$('#f_index').text(combinaison.o);
-		$('#b_index').text(combinaison.p);
-		$('#l_index').text(combinaison.q);
-		$('#g_index').text(combinaison.r);
-		$('#c_index').text(combinaison.s);
+		$('#q').text(combinaison.a);
+		$('#m').text(combinaison.b);
+		$('#h').text(combinaison.c);
+		$('#r').text(combinaison.d);
+		$('#n').text(combinaison.e);
+		$('#i').text(combinaison.f);
+		$('#d').text(combinaison.g);
+		$('#s').text(combinaison.h);
+		$('#o').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#e').text(combinaison.k);
+		$('#a').text(combinaison.l);
+		$('#p').text(combinaison.m);
+		$('#k').text(combinaison.n);
+		$('#f').text(combinaison.o);
+		$('#b').text(combinaison.p);
+		$('#l').text(combinaison.q);
+		$('#g').text(combinaison.r);
+		$('#c').text(combinaison.s);
 	} else if (position == 'd') {
 		/*
 		 a + b + c         = 38
@@ -210,24 +222,24 @@ function printPuzzle(combinaison, position) {
 		 m + n + o + p     = 38
 		 q + r + s         = 38
 		 */
-		$('#h_index').text(combinaison.a);
-		$('#d_index').text(combinaison.b);
-		$('#a_index').text(combinaison.c);
-		$('#m_index').text(combinaison.d);
-		$('#i_index').text(combinaison.e);
-		$('#e_index').text(combinaison.f);
-		$('#b_index').text(combinaison.g);
-		$('#q_index').text(combinaison.h);
-		$('#n_index').text(combinaison.i);
-		$('#j_index').text(combinaison.j);
-		$('#f_index').text(combinaison.k);
-		$('#c_index').text(combinaison.l);
-		$('#r_index').text(combinaison.m);
-		$('#o_index').text(combinaison.n);
-		$('#k_index').text(combinaison.o);
-		$('#g_index').text(combinaison.p);
-		$('#s_index').text(combinaison.q);
-		$('#p_index').text(combinaison.r);
-		$('#l_index').text(combinaison.s);
+		$('#h').text(combinaison.a);
+		$('#d').text(combinaison.b);
+		$('#a').text(combinaison.c);
+		$('#m').text(combinaison.d);
+		$('#i').text(combinaison.e);
+		$('#e').text(combinaison.f);
+		$('#b').text(combinaison.g);
+		$('#q').text(combinaison.h);
+		$('#n').text(combinaison.i);
+		$('#j').text(combinaison.j);
+		$('#f').text(combinaison.k);
+		$('#c').text(combinaison.l);
+		$('#r').text(combinaison.m);
+		$('#o').text(combinaison.n);
+		$('#k').text(combinaison.o);
+		$('#g').text(combinaison.p);
+		$('#s').text(combinaison.q);
+		$('#p').text(combinaison.r);
+		$('#l').text(combinaison.s);
 	}
 }
